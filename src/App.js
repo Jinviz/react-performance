5d9265c5ab6e1dcf23d310e37ead627b00cb9530
Jinwoo Kim <rlawlsdn9583@gmail.com>
2024-01-20 19:39:37 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import A from './components/A';
+import B from './components/B';
 
 function App() {
+  const [value, setValue] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+              .then(response => response.json())
+              .then(posts => setPosts(posts));
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div style={{padding: '1rem'}}>
+              <input
+                    value={value}
+                    onchange={e => setValue(e.target.value)}
+              />
+
+              <div style={{display: 'flex'}}>
+                    <A />
+                    <B />
+              </div>                  
+        </div>
   );
 }
-
 export default App;
